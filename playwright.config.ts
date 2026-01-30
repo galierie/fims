acf771b3083b170ad2e7a48ba40a628ef3a48024
Eriene Galinato <edgalinato@up.edu.ph>
2026-01-31 00:09:36 +0800

    chore: run formatters and linters

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testDir: 'tests'
+    webServer: { command: 'npm run build && npm run preview', port: 4173 },
+    testDir: 'tests',
 });
