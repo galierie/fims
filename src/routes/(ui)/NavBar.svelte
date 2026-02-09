94d3cd77888ccb9c522ea980426acadae5027a93
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 02:49:58 +0800

    feat: make `NavBar`

---
+++ src/routes/(ui)/NavBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { AccountType } from "$lib/types/account-type";
+  import NavBarItem from "./NavBarItem.svelte";
+
+  interface Props {
+    accountType: AccountType;
+    accountColor: string;
+  }
+
+  let { accountType, accountColor }: Props = $props();
+</script>
+
+{#if accountType === AccountType.It}
+  <div class="flex justify-center">
+    <nav class="border-{accountColor} mt-5 border-2 rounded-full text-{accountColor} h-11 flex w-fit px-11 bg-white absolute">
+      <NavBarItem pageName="Accounts" href="/accounts/" />
+      <NavBarItem pageName="Faculty Records" href="/faculty-records/" />
+    </nav>
+  </div>
+{/if}
