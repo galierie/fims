6202689248d0bf9e7aee220bccc7a9d0c352e540
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 02:48:55 +0800

    feat: make `LogOutButton`

---
+++ src/routes/(ui)/LogOutButton.svelte
@@ -0,0 +1,3 @@
+<button class="border-2 border-fims-red bg-white text-fims-red px-9 py-2 hover:bg-fims-red hover:text-white disabled:border-fims-gray disabled:text-fims-gray rounded-full">
+  Log-out
+</button>
