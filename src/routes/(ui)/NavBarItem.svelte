d188699954918ad3177b8270a5e0a933adda58f6
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 02:49:47 +0800

    feat: make `NavBarItem`

---
+++ src/routes/(ui)/NavBarItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/state";
+
+  interface Props {
+    pageName: string;
+    href: string;
+  }
+
+  let { pageName, href } = $props();
+</script>
+
+<div class="flex justify-center px-4 pt-1 h-full {(page.url.pathname === href) ? 'bg-fims-white' : 'bg-white'} hover:bg-fims-white">
+  <div class="w-fit h-full flex items-center border-b-3 {(page.url.pathname === href) ? 'border-b-fims-red' : 'border-b-transparent'}">
+    <a {href} class="font-semibold">{pageName}</a>
+  </div>
+</div>
