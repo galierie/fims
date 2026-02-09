85aa4038a4b7d8464414c9e7fb09e446485ddab2
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 02:50:19 +0800

    feat: add site layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-    import './layout.css';
-    import favicon from '$lib/assets/favicon.svg';
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import { AccountType } from '$lib/types/account-type';
+  import Header from './(ui)/Header.svelte';
+  import Footer from './(ui)/Footer.svelte';
+  import NavBar from './(ui)/NavBar.svelte';
 
-    const { children } = $props();
+  let { data, children } = $props();
+  let { accountType, accountColor, email } = $derived(data);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+{#if accountType === AccountType.None}
+  {@render children()}
+{:else}
+  <Header {accountType} {accountColor} {email} />
+  <NavBar {accountType} {accountColor} />
+  <main class="bg-[#e9e9e9] h-screen">
+    {@render children()}
+  </main>
+  <Footer />
+{/if}
