fff2b5355973b2e59676d595ec4931efc508fcbe
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 02:49:36 +0800

    feat: add initial `Footer` draft

---
+++ src/routes/(ui)/Footer.svelte
@@ -0,0 +1,9 @@
+<div class="bg-fims-red text-white grid grid-cols-3 min-h-98 *:p-5">
+  <div></div>
+  <div>
+    <h1 class="font-semibold text-3xl">Links</h1>
+  </div>
+  <div>
+    <h1 class="font-semibold text-3xl">Contacts</h1>
+  </div>
+</div>
