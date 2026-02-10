531bc51284034ac1013d80e8df7b9cfce765dbb0
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 15:09:05 +0800

    fix(`NavBar`): set records page route to root

--- src/routes/(ui)/NavBar.svelte
+++ src/routes/(ui)/NavBar.svelte
@@ -16,7 +16,7 @@
             class="border-{accountColor} mt-5 rounded-full border-2 text-{accountColor} absolute flex h-11 w-fit bg-white px-11"
         >
             <NavBarItem pageName="Accounts" href="/accounts/" />
-            <NavBarItem pageName="Faculty Records" href="/faculty-records/" />
+            <NavBarItem pageName="Faculty Records" href="/" />
         </nav>
     </div>
 {/if}
