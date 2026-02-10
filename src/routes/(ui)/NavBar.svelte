91e6ddefdced70fa13a2a838d1969ed902041d30
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-10 16:54:39 +0800

    feat(`NavBar`): increase height

--- src/routes/(ui)/NavBar.svelte
+++ src/routes/(ui)/NavBar.svelte
@@ -13,7 +13,7 @@
 {#if accountType === AccountType.It}
     <div class="flex justify-center">
         <nav
-            class="border-{accountColor} mt-5 rounded-full border-2 text-{accountColor} absolute flex h-11 w-fit bg-white px-11"
+            class="border-{accountColor} mt-5 rounded-full border-2 text-{accountColor} absolute flex h-12 w-fit bg-white px-11"
         >
             <NavBarItem pageName="Accounts" href="/accounts/" />
             <NavBarItem pageName="Faculty Records" href="/" />
