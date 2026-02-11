43a2390619951031ad890af4eff3d70a95389fb5
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 03:52:46 +0800

    feat: make `Header`

---
+++ src/routes/(ui)/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import LogOutButton from "./LogOutButton.svelte";
+
+  interface Props {
+    accountColor: string;
+    email: string;
+  }
+
+  let { accountColor, email }: Props = $props();
+</script>
+
+<header class="flex items-center justify-between h-14 w-full px-6 bg-white">
+  <div>
+    <span class="text-fims-green font-semibold">Faculty Information Management System</span>
+  </div>
+  <div>
+    <span class="text-{accountColor} pr-3 font-semibold">{email}</span>
+    <LogOutButton />
+  </div>
+</header>
