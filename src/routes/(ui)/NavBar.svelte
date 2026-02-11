d50d7eb4245ca60821cc5566c52f86d7737d23db
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 04:00:25 +0800

    feat: make `NavBar`

---
+++ src/routes/(ui)/NavBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import NavBarItem from "./NavBarItem.svelte";
+
+  interface Props {
+    canViewAccounts: boolean;
+    accountColor: string;
+  }
+
+  let { canViewAccounts, accountColor }: Props = $props();
+</script>
+
+{#if canViewAccounts}
+  <div class="flex justify-center">
+    <nav class="border-{accountColor} mt-5 border-2 rounded-full text-{accountColor} h-12 flex w-fit px-11 bg-white absolute">
+      <NavBarItem pageName="Accounts" href="/accounts" />
+      <NavBarItem pageName="Faculty Records" href="/" />
+    </nav>
+  </div>
+{/if}
