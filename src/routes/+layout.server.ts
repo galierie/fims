1de97d3abafc31abe2e54e28f7a8babc354a69df
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 04:30:16 +0800

    feat: supply layout with necessary info

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,29 @@
 import { redirect } from '@sveltejs/kit';
+import { getPermissions, getRole } from '$lib/server/db-helpers.js';
 
-export function load({ locals, url }) {
-    if (!locals.user && !url.pathname.startsWith('/login') && !url.pathname.startsWith('/api/auth'))
+export async function load({ locals, url }) {
+    if (!locals.user && !url.pathname.startsWith('/login') && !url.pathname.startsWith('/api/auth')) {
         throw redirect(307, '/login');
+    } else if (locals.user) {
+        const userRole = await getRole(locals.user.id);
+        const { canaddaccount, canmodifyaccount } = await getPermissions(userRole);
 
-    return { user: locals.user };
+        const accountColorMap = new Map();
+        accountColorMap.set('IT', 'fims-red');
+        accountColorMap.set('Admin', 'fims-green');
+
+        return {
+            isLoggedIn: true, // if it's not, then this line shouldn't have been reached
+            email: locals.user.email,
+            canViewAccounts: canaddaccount || canmodifyaccount,
+            accountColor: accountColorMap.get(userRole),
+        };
+    }
+
+    return {
+        isLoggedIn: false,
+        email: '',
+        canViewAccounts: false,
+        accountColor: '',
+    };
 }
