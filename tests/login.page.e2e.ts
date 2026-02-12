96c55be769ceec0b7d8b4b6f9c33de8b2611cda7
nousername010 <gdemail2025@proton.me>
2026-02-12 22:39:59 +0800

    add: made admin login case

--- tests/login.page.e2e.ts
+++ tests/login.page.e2e.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test"
 
-test('successful login', async ({ page }) => {
+const FAIL_ROUTE = "/login?/signInEmail"
+
+test('successful login as it', async ({ page }) => {
     await page.goto("/login");
 
     await expect(page.locator("#email")).toBeVisible()
@@ -10,7 +12,22 @@ test('successful login', async ({ page }) => {
     await page.locator("#password").fill("password")
     await page.locator("#password").press("Enter")
 
-    await expect(page).not.toHaveURL("/login");
+    await expect(page).not.toHaveURL(FAIL_ROUTE);
+    await expect(page.locator(".text-fims-red")).toBeVisible;
+});
+
+test('successful login as admin', async ({ page }) => {
+    await page.goto("/login");
+
+    await expect(page.locator("#email")).toBeVisible();
+    await expect(page.locator("#password")).toBeVisible();
+
+    await page.locator("#email").fill("testadmin@up.edu.ph")
+    await page.locator("#password").fill("adminpass")
+    await page.locator("#password").press("Enter")
+
+    await expect(page).not.toHaveURL(FAIL_ROUTE);
+    await expect(page.locator(".text-fims-green")).toBeVisible;
 });
 
 test('failed login', async ({page}) => {
@@ -22,5 +39,8 @@ test('failed login', async ({page}) => {
     await page.locator("#password").fill("wrongpassword")
     await page.locator("#password").press("Enter")
 
-    await expect(page).toHaveURL("login?/signInEmail");
-})
+    await expect(page).toHaveURL(FAIL_ROUTE);
+
+    //change this expect when the warning has an id
+    await expect(page.locator("p.px-8")).toHaveText("Invalid email or password")
+});
