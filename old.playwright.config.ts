2af569a5fbe6ba5a6e6278adc69303d5b4e9d8e8
nousername010 <gdemail2025@proton.me>
2026-02-15 16:01:06 +0800

    feat: imported test cases

---
+++ old.playwright.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+    webServer: { command: 'npm run build && npm run preview', port: 4173 },
+    testDir: 'tests',
+    testMatch: /.e2e.(?:js|ts)/u,
+    outputDir: 'playwright-results',
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    webServer: { command: 'npm run build && npm run preview', port: 4173 },
-    testDir: 'tests',
-    testMatch: /.e2e.(?:js|ts)/u,
+    webServer: {
+        command: 'pnpm preview',
+        port: 4173,
+        reuseExistingServer: true,
+    },
+    testDir: 'tests/playwright',
     outputDir: 'playwright-results',
+    use: {
+        baseURL: 'http://localhost:4173',
+    },
+    projects: [
+        {
+            name: 'admin-auth',
+            testMatch: /admin-auth.setup.e2e.(?:js|ts)/u,
+        },
+        {
+            name: 'it-auth',
+            testMatch: /it-auth.setup.e2e.(?:js|ts)/u,
+        },
+        {
+            name: 'common-tests',
+            dependencies: ['admin-auth', 'it-auth'],
+            testMatch: /.common.e2e.(?:js|ts)/u,
+        },
+        {
+            name: 'it-specific-tests',
+            dependencies: ['it-auth'],
+            testDir: 'tests/playwright/it-specific',
+            testMatch: /.e2e.(?:js|ts)/u,
+        },
+    ],
 });
