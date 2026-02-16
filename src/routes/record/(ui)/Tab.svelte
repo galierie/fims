c868d072152cc72ac731744bd32f85605f6ad218
nousername010 <gdemail2025@proton.me>
2026-02-16 11:24:11 +0800

    feat: init

---
+++ src/routes/record/(ui)/Tab.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let test:string = "";
+</script>
+
+<div>
+    <span class="mt-60">{test}</span>
+</div>
+
+<style>
+    @reference "tailwindcss";
+</style>
---
+++ src/routes/record/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   import Profile from "./tabs/Profile.svelte" 
+   import Tab from "./(ui)/Tab.svelte"
+</script>
+
+<!-- currently testing components -->
+<div>
+    <Profile/>
+    <Tab test="wow"/>
+</div>
