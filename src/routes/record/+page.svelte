f9228bed367d4f565e1950f37accee1bdd6d70d5
nousername010 <gdemail2025@proton.me>
2026-02-17 22:32:57 +0800

    chore: ui change

--- src/routes/record/+page.svelte
+++ src/routes/record/+page.svelte
@@ -10,8 +10,7 @@
 
 <!-- currently testing components -->
 <div class="mt-20 text-fims-green px-5">
-    <a href={back} class="underline mb-[31px]">Back to List of Faculty Records</a>
-    <h1 class="text-3xl font-semibold mb-[65px]">{lastName}, {firstName}</h1>
+    <h1 class="text-3xl font-semibold mb-[65px]" id="name-display">{lastName}, {firstName}</h1>
     <div class= "flex">
         {#each tabs as tab}
             <div class="flex px-4 h-fit w-fit border-x-2 border-t-2 rounded-t-xl items-center text-xl">
