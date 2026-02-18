8ac4ff8fcacc1d74a9889d7b8714af59031d0019
nousername010 <gdemail2025@proton.me>
2026-02-19 00:39:16 +0800

    chore: change css

--- src/routes/record/(ui)/Field.svelte
+++ src/routes/record/(ui)/Field.svelte
@@ -2,11 +2,11 @@
     let { field="Field", value="Field Value" } = $props();
 </script>
 
-<div class="flex w-full m-5">
-    <div class="bg-fims-green w-20 content-center">
-        <p class="m-auto text-white">{field}</p>
+<div class="flex w-full">
+    <div class="w-50 content-center flex justify-end px-2 items-center">
+        <p class="w-fit text-align-right">{field}</p>
     </div>
-    <div class="bg-white px-2 w-100">
-        <p class="text-black">{value}</p>
+    <div class="bg-white px-2 w-100 rounded-sm flex justify-begin items-center">
+        <p class="">{value}</p>
     </div>
 </div>
