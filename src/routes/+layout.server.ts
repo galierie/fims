9b7e0bf80a119b85e27ee2056dfd31a6bdc9a9ee
nousername010 <gdemail2025@proton.me>
2026-02-19 11:34:11 +0800

    refactor: hide navbar in faculty record

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -25,6 +25,7 @@ export async function load({ locals, url }) {
             isLoggedIn: true, // if it's not, then this line shouldn't have been reached
             email: locals.user.email,
             canViewAccounts: canaddaccount || canmodifyaccount,
+            isViewingRecord: url.pathname.startsWith('/record'),
             canViewChangeLogs,
             accountColor: accountColorMap.get(userRole),
         };
@@ -32,6 +33,7 @@ export async function load({ locals, url }) {
 
     return {
         isLoggedIn: false,
+        isViewingRecord: false,
         email: '',
         canViewAccounts: false,
         canViewChangeLogs: false,
