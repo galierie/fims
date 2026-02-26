771171142654f98f4c63ec393b43f0dca8a087a0
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-26 19:51:43 +0800

    feat(`FacultyRecordRow`): link to record

--- src/routes/(lists)/ui/FacultyRecordRow.svelte
+++ src/routes/(lists)/ui/FacultyRecordRow.svelte
@@ -10,6 +10,7 @@
 
     const { facultyRecord, canViewChangeLogs, isSelected, onToggle }: Props = $props();
     const {
+        facultyid,
         lastname,
         firstname,
         status,
@@ -22,28 +23,32 @@
 
 </script>
 
-<div
-    class="flex justify-center [&>div]:flex [&>div]:h-12 [&>div]:items-center [&>div]:border-b [&>div]:border-fims-gray [&>div]:bg-white [&>div]:px-6"
->
-    <div class="w-25 justify-center">
-        <input
-            type="checkbox"
-            checked={isSelected}
-            onchange={onToggle}
-            class="h-5 w-5 rounded-sm checked:bg-fims-gray focus:ring-0"
-        />
-    </div>
-    <div class={canViewChangeLogs ? 'w-66 2xl:w-132' : 'w-116 2xl:w-182'}>
-        <span><button class="hover:cursor-pointer">{lastname}, {firstname}</button></span>
-    </div>
-    <div class="w-50 justify-center 2xl:w-75"><span>{status}</span></div>
-    <div class="w-62.5 justify-center 2xl:w-75"><span>{ranktitle}</span></div>
-    <div class="w-62.5 justify-center 2xl:w-75">
-        <span>{adminposition ? adminposition : '-'}</span>
-    </div>
-    {#if canViewChangeLogs}
-        <div class="w-50">
-            <span class="truncate text-[#535353]">{logMaker} ({logTimestamp}): {logOperation}</span>
+{#if facultyid !== null}
+    <div
+        class="flex justify-center [&>div]:flex [&>div]:h-12 [&>div]:items-center [&>div]:border-b [&>div]:border-fims-gray [&>div]:bg-white [&>div]:px-6"
+    >
+        <div class="w-25 justify-center">
+            <input
+                type="checkbox"
+                checked={isSelected}
+                onchange={onToggle}
+                class="h-5 w-5 rounded-sm checked:bg-fims-gray focus:ring-0"
+            />
         </div>
-    {/if}
-</div>
+        <a href="/faculty/{facultyid}" class="flex justify-center [&>div]:flex [&>div]:h-12 [&>div]:items-center [&>div]:border-b [&>div]:border-fims-gray [&>div]:bg-white [&>div]:px-6">
+            <div class={canViewChangeLogs ? 'w-66 2xl:w-132' : 'w-116 2xl:w-182'}>
+                <span>{lastname}, {firstname}</span>
+            </div>
+            <div class="w-50 justify-center 2xl:w-75"><span>{status}</span></div>
+            <div class="w-62.5 justify-center 2xl:w-75"><span>{ranktitle}</span></div>
+            <div class="w-62.5 justify-center 2xl:w-75">
+                <span>{adminposition ? adminposition : '-'}</span>
+            </div>
+        </a>
+        {#if canViewChangeLogs}
+            <div class="w-50">
+                <span class="truncate text-[#535353]">{logMaker} ({logTimestamp}): {logOperation}</span>
+            </div>
+        {/if}
+    </div>
+{/if}
