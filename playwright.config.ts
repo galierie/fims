a233587e259e10a2726a1e64b550351b1672137c
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-02-27 22:02:07 +0800

    feat: add test-id to account row; mostly-parallelized tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,6 @@ import dotenv from 'dotenv';
 import { defineConfig } from '@playwright/test';
 dotenv.config({ path: '.env.e2e' }); //change this to desired .env file
 
-function provideReset(tag:number) {return {
-    name: `reset-${tag}`,
-    testMatch: /seed.e2e.(?:js|ts)/u,
-};};
-
 export default defineConfig({
     webServer: {
         command: 'npm run build && npm run preview',
@@ -41,6 +36,7 @@ export default defineConfig({
             name: 'common-tests',
             dependencies: ['admin-auth', 'it-auth', 'invalid-logins'],
             testMatch: /.common.e2e.(?:js|ts)/u,
+            fullyParallel: false,
         },
         // common destructive tests, as they can't be easily parallelized due to deletions and stuff
         {
@@ -49,25 +45,55 @@ export default defineConfig({
             testDir: 'tests/playwright/destructive',
             testMatch: /.e2e.(?:js|ts)/u,
         },
+
+        // i am not exacly sure how to parallelize them without some race condition ocurring
+        // so i just made them two separate files and used test.describe to specifically say which ones
+        // are parallel
         {
-            name: 'it-specific-tests',
-            dependencies: ['common-tests', 'common-destructive-tests'],
-            testDir: 'tests/playwright/it-specific',
+            name: 'it-specific-tests-indiv',
+            dependencies: ['common-tests', 'common-destructive-tests', 'it-auth'],
+            testDir: 'tests/playwright/it-specific/indiv',
             testMatch: /.e2e.(?:js|ts)/u,
         },
         {
-            name: 'it-specific-destructive-tests',
-            dependencies: ['it-specific-tests'],
-            testDir: 'tests/playwright/it-specific/destructive',
+            name: 'it-specific-tests-batch-creation',
+            dependencies: ['it-specific-tests-indiv', 'it-auth'],
+            testDir: 'tests/playwright/it-specific/batch',
+            testMatch: /.creation.e2e.(?:js|ts)/u,
+            fullyParallel: true,
+        },
+        {
+            name: 'it-specific-tests-batch-search',
+            dependencies: ['it-specific-tests-batch-creation', 'it-auth'],
+            testDir: 'tests/playwright/it-specific/batch',
+            testMatch: /.search.e2e.(?:js|ts)/u,
+            fullyParallel: true
+        },
+        {
+            name: 'it-specific-tests-batch-deletion',
+            dependencies: ['it-specific-tests-batch-search', 'it-auth'],
+            testDir: 'tests/playwright/it-specific/batch',
+            testMatch: /.deletion.e2e.(?:js|ts)/u,
+            fullyParallel: false, //cancellation test inside; cancel first then delete
+        },
+ 
+        {
+            name: 'it-specific-tests-generic',
+            dependencies: ['it-auth'],
+            testDir: 'tests/playwright/it-specific/generic',
             testMatch: /.e2e.(?:js|ts)/u,
+            fullyParallel: true,
         },
 
         // logout tests
         {
             name: 'logout',
             dependencies: [
-                'it-specific-destructive-tests',
-                'it-specific-tests',
+                'it-specific-tests-indiv',
+                'it-specific-tests-batch-creation',
+                'it-specific-tests-batch-search',
+                'it-specific-tests-batch-deletion',
+                'it-specific-tests-generic',
                 'common-tests',
                 'common-destructive-tests',
                 'invalid-logins',
