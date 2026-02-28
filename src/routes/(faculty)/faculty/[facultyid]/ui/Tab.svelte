a52011223224924488eb632f37517ad3aeaa4b2a
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-28 18:38:20 +0800

    feat(`Tab`): redesign component

--- src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
@@ -1,7 +1,12 @@
-<script lang="ts">
-    export let test:string = "fewfew";
+<script>
+    import { page } from "$app/state";
+    const { href, name } = $props();
+
+    const unselected = $derived(
+        (page.url.pathname === href)
+            ? ''
+            : 'border-b-2'
+    );
 </script>
 
-<div>
-    <span class="full">{test}</span>
-</div>
+<a {href} class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap {unselected}">{name}</a>
