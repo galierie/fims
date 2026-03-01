a355fb2239e46ef83d9fa1493f084f9aab0d68ec
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-01 12:53:50 +0800

    feat(faculty): add edit, save, discard changes buttons

--- src/routes/(faculty)/faculty/[facultyid]/+page.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
 
-    import { resetViewState } from './states/view-state.svelte.js';
+    import GreenButton from '$lib/ui/GreenButton.svelte';
+    import RedButton from '$lib/ui/RedButton.svelte';
+
+    import { viewState, setToEdit, resetViewState } from './states/view-state.svelte.js';
     
     const { data, form } = $props();
     const { profile } = $derived(data);
@@ -18,3 +21,21 @@
         <p class="px-8">{form.error}</p>
     </div>
 {/if}
+
+<div class="flex items-center gap-2">
+    {#if viewState.isEditing}
+        <GreenButton>
+            <Icon icon="tabler:device-floppy" class="h-5 w-5 mr-2" />
+            <span>Save Record</span>
+        </GreenButton>
+        <RedButton>
+            <Icon icon="tabler:database-off" class="h-5 w-5 mr-2" />
+            <span>Discard Changes</span>
+        </RedButton>
+    {:else}
+        <GreenButton onclick={setToEdit}>
+            <Icon icon="tabler:edit" class="h-5 w-5 mr-2" />
+            <span>Edit</span>
+        </GreenButton>
+    {/if}
+</div>
--- src/routes/(faculty)/faculty/[facultyid]/[ay]/[sem]/+page.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/[ay]/[sem]/+page.svelte
@@ -1,6 +1,11 @@
 <script>
+    import Icon from '@iconify/svelte';
+
+    import GreenButton from '$lib/ui/GreenButton.svelte';
+    import RedButton from '$lib/ui/RedButton.svelte';
+
     import { chooseSemestralRecord } from '../../states/chosen-semestral-record.svelte.js';
-    import { resetViewState } from '../../states/view-state.svelte.js';
+    import { viewState, setToEdit, resetViewState } from '../../states/view-state.svelte.js';
 
     const { data } = $props();
     const { acadYear, semestralRecord, semNum } = $derived(data);
@@ -11,3 +16,21 @@
     // Persist chosen semestral record
     $effect(() => (chooseSemestralRecord(acadYear, semNum)));
 </script>
+
+<div class="flex items-center gap-2">
+    {#if viewState.isEditing}
+        <GreenButton>
+            <Icon icon="tabler:device-floppy" class="h-5 w-5 mr-2" />
+            <span>Save Record</span>
+        </GreenButton>
+        <RedButton>
+            <Icon icon="tabler:database-off" class="h-5 w-5 mr-2" />
+            <span>Discard Changes</span>
+        </RedButton>
+    {:else}
+        <GreenButton onclick={setToEdit}>
+            <Icon icon="tabler:edit" class="h-5 w-5 mr-2" />
+            <span>Edit</span>
+        </GreenButton>
+    {/if}
+</div>
