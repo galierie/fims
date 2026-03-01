6762b4e98ab65fe8a4d142af9e4ddd189bd77a84
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-01 12:49:35 +0800

    feat(`Tab`): disable on editing view state

--- src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
@@ -1,5 +1,6 @@
 <script>
     import { page } from "$app/state";
+    import { viewState } from "../states/view-state.svelte";
     const { href, name } = $props();
 
     const unselected = $derived(
@@ -7,6 +8,12 @@
             ? ''
             : 'border-b-2'
     );
+
+    let disabled = $derived(page.url.pathname !== href && viewState.isEditing);
 </script>
 
-<a {href} class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap {unselected}">{name}</a>
+{#if disabled}
+    <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-gray py-1.5 px-6 text-3xl text-fims-gray w-fit text-nowrap border-b-2 border-b-fims-green">{name}</p>
+{:else}
+    <a {href} class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap {unselected}">{name}</a>
+{/if}
