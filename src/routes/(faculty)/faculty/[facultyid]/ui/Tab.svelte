056fb58da24d2c1dd83a3c184ebf9db1dc62fc57
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-02 22:14:31 +0800

    feat(`Tab`): enable subroute detection

--- src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
@@ -3,10 +3,10 @@
     import { viewState } from "../states/view-state.svelte";
     const { href, name } = $props();
 
-    let disabled = $derived(page.url.pathname !== href && viewState.isEditing);
+    let disabled = $derived(page.url.pathname.includes(href) && viewState.isEditing);
 </script>
 
-{#if page.url.pathname === href}
+{#if page.url.pathname.includes(href)}
     <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap cursor-pointer">{name}</p>
 {:else if disabled}
     <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-gray py-1.5 px-6 text-3xl text-fims-gray w-fit text-nowrap border-b-2 border-b-fims-green cursor-pointer">{name}</p>
