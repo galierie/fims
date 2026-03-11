7dbe4a6a63bfc1bdc6096317e76c0d488b4ae1ca
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-11 14:52:49 +0800

    refactor(faculty/profile): delegate form elements to `ProfileForm`

--- src/routes/(faculty)/faculty/[facultyid]/profile/+page.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/profile/+page.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
-    import { enhance } from '$app/forms';
     import Icon from '@iconify/svelte';
 
-    import GreenButton from '$lib/ui/GreenButton.svelte';
-    import LoadingScreen from '$lib/ui/LoadingScreen.svelte';
-    import RedButton from '$lib/ui/RedButton.svelte';
+    import ProfileForm from '../ui/ProfileForm.svelte';
 
-    import { viewState, setToEdit, resetViewState } from '../states/view-state.svelte.js';
+    import { resetViewState } from '../states/view-state.svelte.js';
     
     const { data, form } = $props();
-    const { profile } = $derived(data);
+    const { profile, opts, dependencyMaps } = $derived(data);
 
     // Ensure view isn't set to editing state on load
     resetViewState();
-
-    let isLoading = $state(false);
-
-    let profileForm: HTMLFormElement | null = null;
-    const profileFormId = 'profile-form';
 </script>
 
 {#if form?.error}
@@ -29,41 +21,4 @@
     </div>
 {/if}
 
-<div class="flex items-center gap-2">
-    {#if viewState.isEditing}
-        <GreenButton onclick={() => {
-            if (profileForm) profileForm.requestSubmit();
-        }}>
-            <Icon icon="tabler:device-floppy" class="h-5 w-5 mr-2" />
-            <span>Save Record</span>
-        </GreenButton>
-        <RedButton>
-            <Icon icon="tabler:database-off" class="h-5 w-5 mr-2" />
-            <span>Discard Changes</span>
-        </RedButton>
-    {:else}
-        <GreenButton onclick={setToEdit}>
-            <Icon icon="tabler:edit" class="h-5 w-5 mr-2" />
-            <span>Edit</span>
-        </GreenButton>
-    {/if}
-</div>
-
-<form
-    method="POST"
-    action="?/update"
-    id={profileFormId}
-    bind:this={profileForm}
-    use:enhance={() => {
-        resetViewState();
-        isLoading = true;
-        return async ({ update }) => {
-            await update();
-            isLoading = false;
-        };
-    }}
-></form>
-
-{#if isLoading}
-    <LoadingScreen />
-{/if}
+<ProfileForm {profile} {opts} {dependencyMaps} />
