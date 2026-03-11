f7985d805a5f7829e2ac881c46254e6d08399f16
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-11 14:49:32 +0800

    feat(faculty): pass rank options and dependency maps from server

--- src/routes/(faculty)/faculty/[facultyid]/profile/+page.server.ts
+++ src/routes/(faculty)/faculty/[facultyid]/profile/+page.server.ts
@@ -1,7 +1,7 @@
 import { error, fail, redirect } from '@sveltejs/kit';
 
 import { deleteFacultyRecords } from '$lib/server/queries/db-helpers';
-import { getAllAppointmentStatuses, getAllFieldsOfInterest, getFacultyProfile } from '$lib/server/queries/faculty-view';
+import { getAllAppointmentStatuses, getAllFieldsOfInterest, getAllRanks, getFacultyProfile } from '$lib/server/queries/faculty-view';
 import { refreshFacultyRecordSearchView } from '$lib/server/queries/faculty-list';
 
 export async function load({ params }) {
@@ -18,11 +18,21 @@ export async function load({ params }) {
 
     // Get input dropdown options and dependency mappings
     const opts: Map<string, Array<any>> = new Map();
+    const dependencyMaps: Map<string, Map<string, string>> = new Map();
 
     opts.set('fieldsOfInterest', await getAllFieldsOfInterest());
     opts.set('appointmentStatuses', await getAllAppointmentStatuses());
 
-    return { profile, opts };
+    const ranks = await getAllRanks();
+    opts.set('rankTitles', ranks.map(({ rankTitle }) => rankTitle));
+
+    const rankTitlesToSalaryGrades: Map<string, string> = new Map(ranks.map(({ rankTitle, salaryGrade }) => [rankTitle, salaryGrade]));
+    dependencyMaps.set('rankTitlesToSalaryGrades', rankTitlesToSalaryGrades);
+
+    const rankTitlesToSalaryRates: Map<string, string> = new Map(ranks.map(({ rankTitle, salaryRate }) => [rankTitle, salaryRate]));
+    dependencyMaps.set('rankTitlesToSalaryRates', rankTitlesToSalaryRates);
+
+    return { profile, opts, dependencyMaps };
 }
 
 export const actions = {
