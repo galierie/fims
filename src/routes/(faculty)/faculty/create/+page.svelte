185b7cb9a4cf693997ea20ce78bb2689b2e391de
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-11 16:06:35 +0800

    feat(faculty/create): add page for faculty profile creation

---
+++ src/routes/(faculty)/faculty/create/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import ProfileForm from "../[facultyid]/ui/ProfileForm.svelte";
+
+    const { data } = $props();
+    const { opts, dependencyMaps } = $derived(data);
+</script>
+
+<main class="bg-[#e9e9e9]">
+    <div class="mx-6 pt-7.5 pb-40">
+        <ProfileForm {opts} {dependencyMaps} isCreating={true} />
+    </div>
+</main>
