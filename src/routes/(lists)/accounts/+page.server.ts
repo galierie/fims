14b45bf1bd842607e0cc375d1375de8674fa17fd
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-04-15 23:40:43 +0800

    feat: change password form action

--- src/routes/(lists)/accounts/+page.server.ts
+++ src/routes/(lists)/accounts/+page.server.ts
@@ -288,6 +288,47 @@ export const actions = {
         }
     },
 
+    async changePassword({locals, request}) {
+        const formData = await request.formData();
+
+        const userid = formData.get('userid') as string;
+        const newPass = formData.get('newpass') as string;
+
+        if (typeof userid === 'undefined') return fail(403, 'No such account');
+        if (typeof newPass === 'undefined') return fail(403, 'Must input a password');
+
+        if (newPass.length === 0) return fail(403, 'Nust input a password');
+
+        //permissions check
+        const [roleObj] = await getUserRoleAndPermissions(locals.user.id);
+        if (typeof roleObj === 'undefined') return fail(403, 'Insufficient Permissions');
+        
+        if (!roleObj.canModifyAccount) return fail(403, 'Insufficient Permissions')
+        
+        try {
+            const response = await auth.api.setUserPassword({
+                body: {
+                    userId: userid,
+                    newPassword: newPass,
+                },
+                headers: request.headers
+            })
+
+            if (!response.status) {
+                return fail(400, 'Failed to change account password')
+            }
+        } catch(error) {
+            console.log(error);
+            return fail(500, {
+                error: error instanceof APIError ? error.message : 'Failed to change account password. (Unknown/Internal error)',
+            });
+        }
+        return {
+            success: true,
+            message: 'Changed account password.',
+        }
+    },
+
     async changeRole({ locals, request }) {
         // Check existing session
         if (typeof locals.user === 'undefined') throw redirect(307, '/login');
