55e9d9fcb17456eeb29db06d4c8cc640089a8b4b
Eriene Galinato <edgalinato@up.edu.ph>
2026-05-05 02:11:56 +0800

    refactor: avoid user id as change password route parameter

--- src/routes/(lists)/accounts/change-password/+page.svelte
+++ src/routes/(lists)/accounts/change-password/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
     import { goto } from '$app/navigation';
     import GreenButton from '$lib/ui/GreenButton.svelte';
     import RedButton from '$lib/ui/RedButton.svelte';
@@ -10,8 +11,8 @@
 <br />
 <div class="flex justify-center">
     <div class="mt-25 flex w-200 flex-col items-stretch p-20 text-center">
-        <h1 class="text-2xl">Change password</h1>
-        <form method="POST" action="?/changePassword" class="mt-5 flex flex-col items-center gap-5">
+        <h1 class="text-2xl">Change Password</h1>
+        <form method="POST" action="?/changePassword" class="mt-5 flex flex-col items-center gap-5" use:enhance>
             <label for="newpass"
                 >New Password: <input name="newpass" type="password" required /></label
             >
